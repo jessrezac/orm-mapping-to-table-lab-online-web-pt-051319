866ef034e52424a2e03cee1ec179512abc128184
Jess Rezac <jess.rezac@gmail.com>
2019-07-18 20:14:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,7 +20,7 @@ class Student
     
     SQL
     
-    DB[conn]
+    DB[:conn].execute(sql)
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
