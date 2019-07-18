617a7dcf1d50ea8916edfd134804be8c86b4f5bc
Jess Rezac <jess.rezac@gmail.com>
2019-07-18 20:23:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,7 +41,8 @@ class Student
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
     
-    def self.create(name, grade)
+    def self.create(attributes)
+      attributes[:name]
       student = Student.new(name, grade)
       student.save
       student
