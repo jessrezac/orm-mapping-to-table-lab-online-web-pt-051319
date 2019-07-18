9a8ff5bc5bbb66c522e326c0c0996bcd6b291e41
Jess Rezac <jess.rezac@gmail.com>
2019-07-18 20:12:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,11 @@ class Student
 
   def self.create_table
     sql = <<SQL
-      
+      CREATE TABLE IF NOT EXIST songs(
+        id INTEGER PRIMARY KEY
+        name TEXT
+        grade INTEGER
+      );
     
     SQL
   # Remember, you can access your database connection anywhere in this class
