3cb47b16493dd96068669f82da3700a70a70ba45
Jess Rezac <jess.rezac@gmail.com>
2019-07-18 20:10:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,17 @@
 class Student
+  
+  attr_accessor :name, :grade
+  attr_reader :id
+  
+  def initialize(name:, grade:)
+    @name = name
+    @grade = grade
+    @id = nil
+  end
 
+
+  def self.create_table
+    
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
