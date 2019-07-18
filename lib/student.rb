a88c49814bdbfae4476623e6d0014ab7fda17064
Jess Rezac <jess.rezac@gmail.com>
2019-07-18 20:20:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
   
-  def initialize(name:, grade:)
+  def initialize(name, grade)
     @name = name
     @grade = grade
     @id = nil
